2e87365ee9eb3ee5ac13546c4646a093e81f1439
Michaela-K <michaelakodes@gmail.com>
2022-03-21 17:14:06 +0000

    update group by query

--- 3_queries_group-by/average_completion_time.sql
+++ 3_queries_group-by/average_completion_time.sql
@@ -1,6 +1,8 @@
-SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
 FROM students
-JOIN assignment_submissions ON students.id = student_id
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON assignments.id = assignment_id
 WHERE end_date IS NULL
 GROUP BY student
-ORDER BY average_assignment_duration DESC;
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
